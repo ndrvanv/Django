73f9a3f158af9e99425e64e3057a88f1dd129af1
Andrew <it.andr@outlook.com>
2023-09-27 18:43:53 +0300

    third homework

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'myapp',
     'myapp2',
+    'myapp3',
 ]
 
 MIDDLEWARE = [
@@ -56,7 +57,9 @@ ROOT_URLCONF = 'myproject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR / "templates",
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from myapp3.views import index
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('prefix/', include('myapp.urls')),
+    path('les3/', include('myapp3.urls')),
+    path('', index),
 ]
