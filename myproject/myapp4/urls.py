d023cc58c3054813047fd0a263805ab939e1da4f
Andrew <it.andr@outlook.com>
2023-10-03 15:22:57 +0300

    4 lesson lection + homework

--- myproject/myapp4/urls.py
+++ myproject/myapp4/urls.py
@@ -7,4 +7,9 @@ urlpatterns = [
     path('forms/', many_fields_form, name='many_fields_form'),
     path('user/', add_user, name='add_user'),
     path('upload', upload_image, name='upload_image')
+
+    path('upload/', views.upload_image, name='upload_image'),
+    path('products/', views.products, name='products'),
+    path('product/<int:product_id>/', views.product, name='product'),
+    path('prod_edit/', views.prod_edit, name='prod_edit'),
 ]
--- myproject/myapp4/views.py
+++ myproject/myapp4/views.py
@@ -1,4 +1,5 @@
 import logging
+from datetime import datetime
 
 from django.core.files.storage import FileSystemStorage
 from django.shortcuts import render
@@ -64,3 +65,145 @@ def upload_image(request):
         form = ImageForm()
     return render(request, 'myapp4/upload_image.html', {'form':
                                                         form})
+
+
+def product(request, product_id):
+    product = get_object_or_404(Product, pk=product_id)
+    products = Product.objects.all()
+    return render(request, 'detail.html', {'product': product, 'products': products})
+
+
+def products(request):
+    logger.info(f'{request} request received')
+    products = Product.objects.all()
+    return render(request, 'catalog.html', {'products': products})
+
+
+def prod_edit(request):
+    logger.info(f'{request} request received')
+    if request.method == 'POST':
+        form = Form6(request.POST, request.FILES)
+        if form.is_valid():
+            name = form.data['name']
+            description = form.data['description']
+            price = form.data['price']
+            prod_quant = form.data['prod_quant']
+            product_id = form.data['product']
+            product = Product.objects.filter(id=product_id).first()
+            img = request.FILES['img']
+            fs = FileSystemStorage()
+            fs.save(img.name, img)
+            product.img = img.name
+            product.name = name
+            product.description = description
+            product.price = price
+            product.prod_quant = prod_quant
+            product.save()
+            return render(request, 'prod_edit.html', {'answer': "Обновлено!"})
+    else:
+        form = Form6()
+    return render(request, 'prod_edit.html', {'form': form})
+
+
+def upload_image(request):
+    if request.method == 'POST':
+        form = ImageForm(request.POST, request.FILES)
+        if form.is_valid():
+            image = form.cleaned_data['image']
+            fs = FileSystemStorage()
+            fs.save(image.name, image)
+            return render(request, 'upload_image.html', {'answer': "Uploaded!"})
+    else:
+        form = ImageForm()
+    return render(request, 'upload_image.html', {'form': form})
+
+
+def united(request, n):
+    context = {'ht': [],
+               'd': [],
+               'r': []}
+    while n > 0:
+        context['ht'].append(random.choice(['Орел', 'Решка']))
+        context['d'].append(random.randint(1, 6))
+        context['r'].append(random.randint(1, 100))
+        n -= 1
+    return render(request, 'united.html', context)
+
+
+def heads_tails(request, n):
+    context = {'res': []}
+    while n > 0:
+        context['res'].append(random.choice(['Орел', 'Решка']))
+        n -= 1
+    return render(request, 'playya.html', context)
+
+
+def dice(request, n):
+    context = {'res': []}
+    while n > 0:
+        context['res'].append(random.randint(1, 6))
+        n -= 1
+    return render(request, 'playya.html', context)
+
+
+def rand(request, n):
+    context = {'res': []}
+    while n > 0:
+        context['res'].append(random.randint(1, 100))
+        n -= 1
+    return render(request, 'playya.html', context)
+
+
+def home(request):
+    context = {'home': 'Homepage'}
+    logger.info(f'{request} request received')
+    return render(request, 'homepage.html', context)
+
+
+def about(request):
+    context = {'about': 'About me'}
+    logger.info(f'{request} request received')
+    return render(request, 'about.html', context)
+
+
+def author_posts(request, author_id):
+    author = get_object_or_404(Author, pk=author_id)
+    posts = Post.objects.filter(author=author).order_by('-id')[:10]
+    return render(request, 'author_posts.html', {'author': author, 'posts': posts})
+
+
+def post_full(request, post_id):
+    post = get_object_or_404(Post, pk=post_id)
+    post.showed += 1
+    post.save()
+    return render(request, 'post_full.html', {'post': post, 'counter': post.showed})
+
+
+def post_comm(request, post_id):
+    post = get_object_or_404(Post, pk=post_id)
+    comments = Comment.objects.filter(post=post).all()
+    post.showed += 1
+    post.save()
+    return render(request, 'post_comment.html', {'post': post, 'comments': comments, 'counter': post.showed})
+
+
+def basket(request, user_id):
+    products = []
+    user = User.objects.filter(pk=user_id).first()
+    orders = Order.objects.filter(customer=user).all()
+    for order in orders:
+        products.append(order.products.all())
+    products.reverse()
+    return render(request, 'basket.html', {'user': user, 'orders': orders, 'products': products})
+
+
+def sorted_basket(request, user_id, days_ago):
+    products = []
+    now = datetime.now()
+    before = now - timedelta(days=days_ago)
+    user = User.objects.filter(pk=user_id).first()
+    orders = Order.objects.filter(customer=user, date_ordered__range=(before, now)).all()
+    for order in orders:
+        products.append(order.products.all())
+    products.reverse()
+    return render(request, 'sorted_basket.html', {'user': user, 'orders': orders, 'products': products})
